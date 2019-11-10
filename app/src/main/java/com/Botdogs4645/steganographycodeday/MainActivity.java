16479c21cc5de95be04031b99c13f8db0ba26525
Oli <oskosiacki@cps.edu>
2019-11-10 07:22:02 -0600

    made the 2 tabs actually functionable

--- app/src/main/java/com/Botdogs4645/steganographycodeday/MainActivity.java
+++ app/src/main/java/com/Botdogs4645/steganographycodeday/MainActivity.java
@@ -18,18 +18,25 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import java.util.ArrayList;
 import java.io.IOException;
+import java.lang.Math;
+
 
 public class MainActivity extends AppCompatActivity {
     private int intentRequestCode = 1;
     private TextView mTextMessage;
     private ImageView imageEncryptIcon;
+    private ImageView newimage;
     private Button encryptButton;
+    private Button decryptButton;
     private EditText secretMessage;
+    private ImageView imageDecryptIcon;
+
 
     private String payload;
     private Bitmap bitmap;
+    private Bitmap newbitmap;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -38,9 +45,19 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_encrypt:
                     mTextMessage.setText(R.string.title_encrypt);
+                    imageEncryptIcon.setVisibility(View.VISIBLE);
+                    imageDecryptIcon.setVisibility(View.INVISIBLE);
+                    encryptButton.setVisibility(View.VISIBLE);
+                    decryptButton.setVisibility(View.INVISIBLE);
+
                     return true;
                 case R.id.navigation_decrypt:
                     mTextMessage.setText(R.string.title_decrypt);
+                    imageEncryptIcon.setVisibility(View.INVISIBLE);
+                    imageDecryptIcon.setVisibility(View.VISIBLE);
+                    encryptButton.setVisibility(View.INVISIBLE);
+                    decryptButton.setVisibility(View.VISIBLE);
+
                     return true;
 
             }
@@ -48,7 +65,6 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-   // protected Bitmap encryptFunction (String userInput, Bitmap userImage){}
 
     public static int calculateInSampleSize(
             BitmapFactory.Options options, int reqWidth, int reqHeight) {
@@ -67,15 +83,20 @@ public class MainActivity extends AppCompatActivity {
         return inSampleSize;
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
-        imageEncryptIcon = (ImageView) findViewById(R.id.imageView);
+        imageEncryptIcon = (ImageView) findViewById(R.id.encryptionImageSelector);
+        imageDecryptIcon = (ImageView) findViewById(R.id.decryptionImageSelector);
         encryptButton = (Button) findViewById(R.id.encryptButton);
+        decryptButton = (Button) findViewById(R.id.decryptButton);
         secretMessage = (EditText) findViewById(R.id.secretMessage);
+        newimage = (ImageView) findViewById(R.id.newimage);
+
 
         imageEncryptIcon.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,8 +113,46 @@ public class MainActivity extends AppCompatActivity {
         encryptButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(v.getContext(), "Encoding, Please Wait A Moment...", Toast.LENGTH_SHORT).show();
-                secretMessage.getText().toString();
+                if (bitmap != null) {
+                    Toast.makeText(v.getContext(), "Encoding, Please Wait A Moment...", Toast.LENGTH_SHORT).show();
+                    payload = secretMessage.getText().toString();
+                    char[] letters = payload.toCharArray();
+                    int pixels[] = new int[bitmap.getWidth() * bitmap.getHeight()];
+                    int pix = 0;
+                    ArrayList<Integer> color = new ArrayList<Integer>();
+
+
+                    int letter = 0;
+                    while (letter < letters.length) {
+
+                        color.add((int) letters[letter]);
+                        if (letter + 1 == letters.length) {
+                            for (int i = color.size(); i < 3; i++) {
+                                color.add(0);
+                            }
+                        }
+                        if (color.size() >= 3) {
+                            pixels[pix] = ((color.get(0) & 0x0ff) << 16) |
+                                    ((color.get(1) & 0x0ff) << 8) |
+                                    (color.get(2) & 0x0ff);
+
+                            color.clear();
+
+
+                        }
+                        letter++;
+
+                    }
+                    for (int i = 0; i < pixels.length; i++) {
+                        if (pixels[i] == 0) {
+                            pixels[i] = bitmap.getPixel(i % bitmap.getWidth(), (int) Math.floor(i / bitmap.getWidth()));
+                        }
+                    }
+                    newbitmap = bitmap.copy(bitmap.getConfig(), true);;
+                    newbitmap.setPixels(pixels, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+                    newimage.setImageBitmap(newbitmap);
+                    newimage.setBackground(null);
+                }
             }
         });
     }
@@ -102,7 +161,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Log.d("hello", "");
         if (resultCode == RESULT_OK) {
             Uri imageUri = data.getData();
             try {
